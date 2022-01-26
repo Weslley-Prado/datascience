519a052a52827002bbc822d1fd731c0feea86859
Weslley Prado <weslley.prado73@gmail.com>
2022-01-26 16:46:11 -0300

    This code will be update and modify in future

--- Examples initial/firstExample1.py
+++ Examples initial/firstExample1.py
@@ -1,6 +1,4 @@
-
 # Inicialize o dict com uma lista vazia para cada id de usuário:
-
 friendships = {user[“id”]: [] for user in users}
 
 # Em seguida, execute um loop pelos pares de amigos para preenchê-la:
@@ -11,7 +9,7 @@ friendships[j].append(i) # Adicione i como amigo do usuário j
 
 def number_of_friends(user):
 
-“““Quantos amigos tem o _user_”””
+“““Quantos amigos tem o _user_?”””
 
 user_id = user[“id”]
 
@@ -21,4 +19,207 @@ return len(friend_ids)
 
 total_connections = sum(number_of_friends(user)
 
-for user in users)
+for user in users) # 24
+
+Em seguida, basta dividir pelo número de usuários:
+
+num_users = len(users) # tamanho da lista de usuários
+avg_connections = total_connections / num_users # 24 / 10 == 2.4
+
+# Crie uma lista (user_id, number_of_friends).
+
+num_friends_by_id = [(user[“id”], number_of_friends(user))
+
+for user in users]
+
+num_friends_by_id.sort( # Classifique a lista
+
+key=lambda id_and_friends: id_and_friends[1], # por num_friends
+
+reverse=True) # do maior para o menor
+
+# Cada par é (user_id, num_friends):
+
+# [(1, 3), (2, 3), (3, 3), (5, 3), (8, 3),
+
+# (0, 2), (4, 2), (6, 2), (7, 2), (9, 1)]
+
+def foaf_ids_bad(user):
+
+“““foaf significa “friend of a friend” [amigo de um amigo] ”””
+
+return [foaf_id
+
+for friend_id in friendships[user[“id”]]
+
+for foaf_id in friendships[friend_id]]
+
+print(friendships[0]) # [1, 2]
+
+print(friendships[1]) # [0, 2, 3]
+
+print(friendships[2]) # [0, 1, 3]
+
+from collections import Counter # não é carregado por padrão
+
+def friends_of_friends(user):
+
+user_id = user[“id”]
+
+return Counter(
+
+foaf_id
+
+for friend_id in friendships[user_id] # Para cada amigo meu,
+
+for foaf_id in friendships[friend_id] # encontre os amigos deles
+
+if foaf_id != user_id # que não sejam eu
+
+and foaf_id not in friendships[user_id] # e não sejam meus amigos.
+
+)
+
+interests = [
+
+(0, “Hadoop”), (0, “Big Data”), (0, “HBase”), (0, “Java”),
+
+(0, “Spark”), (0, “Storm”), (0, “Cassandra”),
+
+(1, “NoSQL”), (1, “MongoDB”), (1, “Cassandra”), (1, “HBase”),
+
+(1, “Postgres”), (2, “Python”), (2, “scikit-learn”), (2, “scipy”),
+
+(2, “numpy”), (2, “statsmodels”), (2, “pandas”), (3, “R”), (3, “Python”),
+
+(3, “statistics”), (3, “regression”), (3, “probability”),
+
+(4, “machine learning”), (4, “regression”), (4, “decision trees”),
+
+(4, “libsvm”), (5, “Python”), (5, “R”), (5, “Java”), (5, “C++”),
+
+(5, “Haskell”), (5, “programming languages”), (6, “statistics”),
+
+(6, “probability”), (6, “mathematics”), (6, “theory”),
+
+(7, “machine learning”), (7, “scikit-learn”), (7, “Mahout”),
+
+(7, “neural networks”), (8, “neural networks”), (8, “deep learning”),
+
+(8, “Big Data”), (8, “artificial intelligence”), (9, “Hadoop”),
+
+(9, “Java”), (9, “MapReduce”), (9, “Big Data”)
+
+]
+
+“““Encontre os ids dos usuários com o mesmo interesse.”””
+
+return [user_id
+
+for user_id, user_interest in interests
+
+if user_interest == target_interest]
+
+from collections import defaultdict
+
+# As chaves são interesses, os valores são listas de user_ids com o interesse em questão
+
+user_ids_by_interest = defaultdict(list)
+
+for user_id, interest in interests:
+
+user_ids_by_interest[interest].append(user_id)
+
+E outro índice, de usuários para interesses:
+
+# As chaves são user_ids, os valores são listas de interesses do user_id em questão.
+
+interests_by_user_id = defaultdict(list)
+
+for user_id, interest in interests:
+
+interests_by_user_id[user_id].append(interest)
+
+def most_common_interests_with(user):
+
+return Counter(
+
+interested_user_id
+
+for interest in interests_by_user_id[user[“id”]]
+
+for interested_user_id in user_ids_by_interest[interest]
+
+if interested_user_id != user[“id”]
+
+)
+
+salaries_and_tenures = [(83000, 8.7), (88000, 8.1),
+
+(48000, 0.7), (76000, 6),
+
+(69000, 6.5), (76000, 7.5),
+
+(60000, 2.5), (83000, 10),
+
+(48000, 1.9), (63000, 4.2)]
+
+# As chaves são anos, os valores são listas de salários por anos de experiência.
+
+salary_by_tenure = defaultdict(list)
+
+for salary, tenure in salaries_and_tenures:
+salary_by_tenure[tenure].append(salary)
+
+# As chaves são anos, cada valor é o salário médio associado ao número de anos de experiência.
+
+average_salary_by_tenure = {
+
+tenure: sum(salaries) / len(salaries)
+
+for tenure, salaries in salary_by_tenure.items()
+
+}
+
+def tenure_bucket(tenure):
+
+if tenure < 2:
+
+return “less than two”
+
+elif tenure < 5:
+
+return “between two and five”
+
+else:
+
+return “more than five”
+
+# As chaves são buckets de anos de experiência, os valores são as listas de salários associadas ao bucket em questão.
+
+salary_by_tenure_bucket = defaultdict(list)
+
+for salary, tenure in salaries_and_tenures:
+
+bucket = tenure_bucket(tenure)
+
+salary_by_tenure_bucket[bucket].append(salary)
+
+# As chaves são buckets de anos de experiência, os valores são a média salarial do bucket em questão.
+average_salary_by_bucket = {
+
+tenure_bucket: sum(salaries) / len(salaries)
+
+for tenure_bucket, salaries in salary_by_tenure_bucket.items()
+
+}
+
+
+
+
+
+
+
+
+
+
